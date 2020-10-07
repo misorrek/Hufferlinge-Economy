fe324340e8c0e1269e4541fbb9faf4f358267d26
Max <maximiliangroiser@gmail.com>
2020-10-07 14:52:27 +0200

    Update InventoryListener.java
    Started refactoring

--- src/huff/economy/listener/InventoryListener.java
+++ src/huff/economy/listener/InventoryListener.java
@@ -41,7 +41,7 @@ public class InventoryListener
 	private final EconomyInterface economy;
 	
 	@EventHandler (priority = EventPriority.HIGHEST) //TODO Handle full inventory
-	public void onBlockHumanInventoryClick(InventoryClickEvent event)
+	public void onDenyHumanInventoryClick(InventoryClickEvent event)
 	{
 		final InventoryType inventoryType = event.getClickedInventory().getType();
 		final InventoryAction inventoryAction = event.getAction();
@@ -71,7 +71,7 @@ public class InventoryListener
 	}
 	
 	@EventHandler (priority = EventPriority.HIGH)
-	public void onEconomyHumanInverntoryClick(InventoryClickEvent event)
+	public void onHumanInverntoryClick(InventoryClickEvent event)
 	{	
 		final ItemStack cursorItem = event.getCursor();
 		final ItemStack slotItem = event.getView().getItem(event.getRawSlot());
@@ -113,143 +113,196 @@ public class InventoryListener
 	}
 	
 	@EventHandler
-	public void onWalletInventoryClick(InventoryClickEvent event)
+	public void onEconomyStorageInventoryClick(InventoryClickEvent event)
 	{
 		final HumanEntity human = event.getWhoClicked();
 		final InventoryView view = event.getView();
 		final String viewTitle = view.getTitle();
 		
-		if (viewTitle.equals(economy.getConfig().getWalletInventoryName()))
+		if (viewTitle.equals(economy.getConfig().getWalletInventoryName()) || viewTitle.equals(economy.getConfig().getBankInventoryName()))
 		{
 			event.setCancelled(true);
-			
-			handleWalletInventoryActions(human, view, event.getCurrentItem());
-		}
-		else if (viewTitle.equals(economy.getConfig().getTransactionInventoryName(TransactionKind.WALLET_OUT)))
-		{		
-			event.setCancelled(true);
-			
-			final ItemStack currentItem = event.getCurrentItem();
 			
 			if (currentItem == null)
 			{
 				return;
 			}
-			String currentItemName = currentItem.getItemMeta().getDisplayName();
+			final String currentItemName = currentItem.getItemMeta().getDisplayName();
 			
-			if (currentItemName.equals(InventoryHelper.ITEM_ABORT))
+			if (currentItemName.equals(InventoryHelper.ITEM_CLOSE))
 			{
-				human.closeInventory();
-				human.openInventory(EconomyUtil.getWalletInventory(economy.getConfig(), economy.getStorage().getWallet(human.getUniqueId())));
+				view.close();
 			}
-			else if (currentItemName.equals(EconomyUtil.getPerformItemName(TransactionKind.WALLET_OUT)))
+			else
 			{
-				final Player targetPlayer = EconomyUtil.getPayTargetPlayer(event.getInventory());
-				
-				if (targetPlayer != null)
-				{
-					if (targetPlayer.isOnline())
-					{
-						final double valueAmount = EconomyUtil.getPayValueAmount(event.getInventory());
-						final String formattedValueAmount = MessageHelper.getHighlighted(economy.getConfig().getValueFormatted(valueAmount));
-						
-						if (economy.getStorage().updateWallet(human.getUniqueId(), valueAmount, true) == EconomyStorage.CODE_SUCCESS &&
-							economy.getStorage().updateWallet(targetPlayer.getUniqueId(), valueAmount, false) == EconomyStorage.CODE_SUCCESS)
-						{
-							human.closeInventory();
-							human.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "an",
-					                                             MessageHelper.getHighlighted(targetPlayer.getName()), "übergeben."));
-							targetPlayer.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "von",
-					                                                    MessageHelper.getHighlighted(human.getName()), "erhalten."));
-							return;
-						}						
-					}
-				}
-				else
-				{
-					double valueAmount = EconomyUtil.getPayValueAmount(event.getInventory());
-					final String formattedValueAmount = MessageHelper.getHighlighted(economy.getConfig().getValueFormatted(valueAmount));
-					final ItemStack valueItem = economy.getConfig().getValueItem();
-					
-					if (economy.getStorage().updateWallet(human.getUniqueId(), valueAmount, true) == EconomyStorage.CODE_SUCCESS)
-					{
-						int maxStackSize = valueItem.getMaxStackSize();
-						
-						while ((int) valueAmount > 0)
-						{
-							if ((int) valueAmount >= maxStackSize)
-							{
-								valueItem.setAmount(maxStackSize);
-								applyLore(valueItem, economy.getSignature().createSignatureLore(maxStackSize));
-								valueAmount -= maxStackSize;
-							}
-							else
-							{
-								valueItem.setAmount((int) valueAmount);
-								applyLore(valueItem, economy.getSignature().createSignatureLore((int) valueAmount));
-								valueAmount = 0;
-							}						
-							human.getInventory().addItem(valueItem);
-						}
-						human.closeInventory();
-						human.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "aus deinem ",
-								                             economy.getConfig().getWalletName(), " herausgenommen."));
-						return;
-					}		
-				}
-				human.closeInventory();
-				human.sendMessage(MessageHelper.PREFIX_HUFF + "Das Herausnehmen des Geldes konnte nicht abgeschlossen werden.");		
-			}
-			else if (StringHelper.contains(false, currentItemName, "+", "-"))
-			{
-				try
-				{
-					final Pattern valuePattern = Pattern.compile("§.([+-]) ([0-9]*)");
-					final Matcher matcher = valuePattern.matcher(currentItemName);				
-					
-					while (matcher.find())
-					{					
-						boolean negativeChange = matcher.group(1).equals("-");
-						double valueChange = Double.parseDouble(matcher.group(2));
-						double valueAmount = EconomyUtil.getPayValueAmount(event.getInventory());
-						double valueSum = valueAmount + (negativeChange ?  valueChange * -1 : valueChange);
-						double currentWallet = economy.getStorage().getWallet(human.getUniqueId());
-						
-						if (valueSum < 0)
-						{
-							if (valueAmount > 0)
-							{
-								valueSum = 0;
-							}
-							else
-							{
-								((Player) human).playSound(human.getLocation(), Sound.ENTITY_EGG_THROW, 1, 2);
-								return;
-							}
-						}	
-						else if (valueSum > currentWallet)
-						{
-							if (valueAmount < currentWallet)
-							{
-								valueSum = currentWallet;
-							}
-							else
-							{
-								((Player) human).playSound(human.getLocation(), Sound.ENTITY_EGG_THROW, 1, 2);
-								return;
-							}
-						}
-						EconomyUtil.setPayValueAmount(event.getInventory(), economy.getConfig().getValueFormatted(valueSum)); 
-						((Player) human).playSound(human.getLocation(), (negativeChange ? Sound.BLOCK_WOODEN_BUTTON_CLICK_OFF : Sound.ENTITY_EXPERIENCE_ORB_PICKUP), 1, 2);
-					}
-				}
-				catch (NumberFormatException exception)
-				{
-					Bukkit.getLogger().log(Level.WARNING, "The value-change is invalid.", exception);
-				}
+				handleTransactionOpen(player, currentItemName)
 			}
 		}
 	}
+	
+	public void handleTransactionOpen(@NotNull Player player, @NotNull String currentItemName)
+	{
+		if (isTransaction(currentItemName))
+		{
+			final TransactionKind transactionKind = TransactionKind.valueOf(currentItemName);
+			
+			if (transactionKind == TransactionKind.BANK_OTHER)
+			{
+				//TODO Open Player Chooser
+			}
+			else
+			{		
+				player.closeInventory();
+				player.openInventory(EconomyUtil.getTransactionInventory(economy.getConfig(), , economy.getStorage().getWallet(player.getUniqueId()), null));
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onTransactionInventoryClick(InventoryClickEvent event)
+	{
+		final HumanEntity human = event.getWhoClicked();
+		final InventoryView view = event.getView();
+		final String viewTitle = view.getTitle();
+		final TransactionKind transactionKind = TransactionKind.getTransaction(viewTitle);
+				
+		if (transactionKind == null)
+		{
+			return;
+		}		
+		event.setCancelled(true);
+		
+		final ItemStack currentItem = event.getCurrentItem();
+		
+		if (currentItem == null)
+		{
+			return;
+		}
+		String currentItemName = currentItem.getItemMeta().getDisplayName();
+		
+		if (currentItemName.equals(InventoryHelper.ITEM_ABORT))
+		{
+			human.closeInventory();
+			human.openInventory(EconomyUtil.getWalletInventory(economy.getConfig(), economy.getStorage().getWallet(human.getUniqueId())));
+			
+			//TODO Allgemein Lösung in Lib (InventoryOpenEvent/InventoryCloseEvent -> Solange speichern bis komplett Close)
+		}
+		else if (StringHelper.contains(false, currentItemName, "+", "-"))
+		{
+			modifyTransactionValue(event.getInventory(), (Player) human, transactionKind.isBankTransaction() ? economy.getStorage.getBank(human.getUniqueId() : economy.getStorage.getWallet(human.getUniqueId()));
+		}
+		else if (StringUtils.containsIgnoreCase(currentItemName, transactionKind.getLabel()))
+		{		
+			final Player targetPlayer = EconomyUtil.getPayTargetPlayer(event.getInventory());
+			
+			if (targetPlayer != null) // 2 Kinds FROM BANK IN BANK / FROM WALLET IN WALLET
+			{
+				if (targetPlayer.isOnline())
+				{
+					final double valueAmount = EconomyUtil.getPayValueAmount(event.getInventory());
+					final String formattedValueAmount = MessageHelper.getHighlighted(economy.getConfig().getValueFormatted(valueAmount));
+					
+					final int selfFeedbackcode = transactionKind.isBankTransaction() ? economy.getStorage().updateBank(human.getUniqueId(), valueAmount, true) : economy.getStorage().updateWallet(human.getUniqueId(), valueAmount, true)
+					final int otherFeedbackcode = transactionKind.isBankTransaction() ?economy.getStorage().updateBank(targetPlayer.getUniqueId(), valueAmount, false) : economy.getStorage().updateWallet(targetPlayer.getUniqueId(), valueAmount, false)
+					
+					if (selfFeedbackcode == EconomyStorage.CODE_SUCCESS && otherFeedbackcode == EconomyStorage.CODE_SUCCESS)
+					{
+						human.closeInventory();
+						human.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "an",
+															 MessageHelper.getHighlighted(targetPlayer.getName()), "übergeben."));
+						targetPlayer.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "von",
+																	MessageHelper.getHighlighted(human.getName()), "erhalten."));
+						return;
+					}						
+				}
+			}
+			else // 2 Kinds FROM WALLET IN INV / FROM BANK IN WALLET
+			{
+					double valueAmount = EconomyUtil.getPayValueAmount(event.getInventory());
+				final String formattedValueAmount = MessageHelper.getHighlighted(economy.getConfig().getValueFormatted(valueAmount));
+				final ItemStack valueItem = economy.getConfig().getValueItem();
+				
+				if (economy.getStorage().updateWallet(human.getUniqueId(), valueAmount, true) == EconomyStorage.CODE_SUCCESS)
+				{
+					int maxStackSize = valueItem.getMaxStackSize();
+					
+					while ((int) valueAmount > 0)
+					{
+						if ((int) valueAmount >= maxStackSize)
+						{
+							valueItem.setAmount(maxStackSize);
+							applyLore(valueItem, economy.getSignature().createSignatureLore(maxStackSize));
+							valueAmount -= maxStackSize;
+						}
+						else
+						{
+							valueItem.setAmount((int) valueAmount);
+							applyLore(valueItem, economy.getSignature().createSignatureLore((int) valueAmount));
+							valueAmount = 0;
+						}						
+						human.getInventory().addItem(valueItem);
+					}
+					human.closeInventory();
+					human.sendMessage(StringHelper.build(MessageHelper.PREFIX_HUFF, "Du hast", formattedValueAmount, "aus deinem ",
+														 economy.getConfig().getWalletName(), " herausgenommen."));
+					return;
+				}		
+			}
+			human.closeInventory();
+			human.sendMessage(MessageHelper.PREFIX_HUFF + "Das Herausnehmen des Geldes konnte nicht abgeschlossen werden.");	
+		}
+		
+	}
+	
+	private void modifyTransactionValue(@NotNull Inventory transactionInventory, @NotNull Player player, double currentStorageValue)
+	{
+		try
+		{
+			final Pattern valuePattern = Pattern.compile("§.([+-]) ([0-9]*)");
+			final Matcher matcher = valuePattern.matcher(currentItemName);				
+			
+			if (matcher.find())
+			{					
+				final boolean negativeChange = matcher.group(1).equals("-");
+				final double changeValue = Double.parseDouble(matcher.group(2));
+				final double currentValue = EconomyUtil.getPayValueAmount(transactionInventory); //TODO Inspect
+				final double summedUpValue = currentValue + (negativeChange ?  changeValue * -1 : changeValue);
+				
+				if (summedUpValue < 0)
+				{
+					if (currentValue > 0)
+					{
+						summedUpValue = 0;
+					}
+					else
+					{
+						player.playSound(player.getLocation(), Sound.ENTITY_EGG_THROW, 1, 2); //TODO Can Human playSound?
+						return;
+					}
+				}	
+				else if (summedUpValue > currentStorageValue)
+				{
+					if (currentValue < currentStorageValue)
+					{
+						summedUpValue = currentStorageValue;
+					}
+					else
+					{
+						player.playSound(player.getLocation(), Sound.ENTITY_EGG_THROW, 1, 2);
+						return;
+					}
+				}
+				EconomyUtil.setPayValueAmount(event.getInventory(), economy.getConfig().getValueFormatted(summedUpValue)); //TODO Inspect
+				player.playSound(player.getLocation(), (negativeChange ? Sound.BLOCK_WOODEN_BUTTON_CLICK_OFF : Sound.ENTITY_EXPERIENCE_ORB_PICKUP), 1, 2);
+			}
+		}
+		catch (NumberFormatException exception)
+		{
+			Bukkit.getLogger().log(Level.WARNING, "The transaction-change-value is invalid.", exception);
+		}
+	}
+	
+	private void m
 	
 	private void applyLore(@NotNull ItemStack loreItem, @NotNull List<String> lore)
 	{
@@ -260,20 +313,6 @@ public class InventoryListener
 	
 	private void handleWalletInventoryActions(@NotNull HumanEntity player, @NotNull InventoryView view, @Nullable ItemStack currentItem)
 	{	
-		if (currentItem == null)
-		{
-			return;
-		}
-		final String currentItemName = currentItem.getItemMeta().getDisplayName();
 		
-		if (currentItemName.equals(InventoryHelper.ITEM_CLOSE))
-		{
-			view.close();
-		}
-		else if (currentItemName.equals(economy.getConfig().getTransactionInventoryName(TransactionKind.WALLET_OUT)))
-		{
-			player.closeInventory();
-			player.openInventory(EconomyUtil.getTransactionInventory(economy.getConfig(), TransactionKind.WALLET_OUT, economy.getStorage().getWallet(player.getUniqueId()), null));
-		}
 	}
 }
